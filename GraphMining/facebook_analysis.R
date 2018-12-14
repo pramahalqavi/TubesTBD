3205fb074ef9894d8d1459106160d7b78094898b
pramahalqavi <pramahalqavi97@gmail.com>
2018-12-14 20:56:39 +0700

    edit transitivity and community detection graph mining

--- GraphMining/facebook_analysis.R
+++ GraphMining/facebook_analysis.R
@@ -8,8 +8,8 @@ net <- as.undirected(net)
 # Density - The proportion of present edges from all possible edges in the network.
 edge_density(net, loops=F)
 
-# Transitivity - global - ratio of triangles (direction disregarded) to connected triples.
-transitivity(net, type="global")
+# Transitivity - This is simply the ratio of the triangles and the connected triples in the graph.
+transitivity(net, type="average")
 
 # Diameter - A network diameter is the longest geodesic distance (length of the shortest path between two nodes) in the network.
 diameter(net, directed=F, weights=NA)
@@ -21,17 +21,17 @@ deg <- degree(net, mode="all")
 hist(deg, breaks=1:1100, main="Histogram of node degree")
 
 # Community detection - Detect groups that consist of densely connected nodes with fewer connections across groups.
-# based on propagating labels
-clp <- cluster_label_prop(net)
-plot(clp, net)
-
-class(clp)
-length(clp)
-membership(clp)
-modularity(clp) # how modular the graph partitioning is
-
 # based on greedy optimization of modularity
 cfg <- cluster_fast_greedy(as.undirected(net))
 plot(cfg, as.undirected(net))
+
+class(cfg)
+length(cfg)
+membership(cfg)
+modularity(cfg)
+
+
+
+
 
 
